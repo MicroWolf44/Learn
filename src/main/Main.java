8abbd993deab43e970b4d9462492cb555a3d1c8a
MicroWolf44 <90967716+MicroWolf44@users.noreply.github.com>
2021-10-20 22:26:31 +0300

    git branch

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import main.lissen1.Primer1;
+
+public class Main{
+
+    static Primer1 l1p1 = new Primer1();
+
+    public static void main(String args[]){
+
+        l1p1.Primer1();
+
+    }
+
+}
---
+++ src/main/lissen1/Primer1.java
@@ -0,0 +1,11 @@
+package main.lissen1;
+
+public class Primer1 {
+
+    public void Primer1(){
+
+        System.out.println("Привет");
+
+    }
+
+}
